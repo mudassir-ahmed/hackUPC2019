4996cd9c3a0dc1fd80c952afcbfbc2c0aff1d216
Chen Bo Calvin Zhang <chen.zhang-10@student.manchester.ac.uk>
2019-10-12 19:25:31 +0200

    added victory page

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link>|
       <router-link to="/loading">Loading</router-link>|
+      <router-link to="/winLose">WinLose</router-link>|
       <router-link to="/about">About</router-link>|
       <router-link :to="{name: 'game'}">Game</router-link>
     </div>
---
+++ src/views/WinLose.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="winLose">
+    Your Team Won!
+    <CharacterIcon state="idle" color='red' />
+  </div>
+</template>
+
+<script>
+import CharacterIcon from '@/components/CharacterIcon.vue';
+
+export default {
+  components: {
+    CharacterIcon,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.winLose {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  font-size:30px;
+  font-weight: "bold";
+}
+</style>
